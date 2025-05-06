6ea00c456699e7d7c39530fc9cd8bf23fc9d06be
HP_Pav <vinaybagde248@gmail.com>
2025-05-06 18:30:03 +0530

    Calorie count feature

--- step_counter.py
+++ step_counter.py
@@ -4,6 +4,8 @@ diff_step = 10000 - step_count
 
 if step_count > 10000 :
     print(f"Keep it up !!!! You have walked {-diff_step} steps more than your goal")
+    print(f" 🥳 You have {-diff_step*0.04} extra calories today. ")
 else :
     print(f"Please walk {diff_step} steps to acheive your goal  ")
+    print(f"You need to burn {diff_step*0.04} calories to achieve your goal 🤗.")
 
